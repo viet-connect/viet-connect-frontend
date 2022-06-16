96ac7bec65ebe68a8ac5565c6a129cac8fa36b45
hyunwook <shw@itemscout.io>
2022-06-17 00:49:37 +0900

    Feat: Next.js 초기세팅 및 Prettier/eslint 세팅 진행중

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home(home) {
+  console.log("1");
+  console.log9('1');
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  )
+}
