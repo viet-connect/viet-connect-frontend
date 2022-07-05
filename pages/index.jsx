2670a933a5402f0e0d6d57aeb68810823573a285
rkdrud34 <rkdrud34@nate.com>
2022-07-06 04:08:43 +0900

    feat:styled-components, antd 테스트

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,31 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
+import styled from 'styled-components';
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default function Home(home) {
   return (
-    <Row>
-      <Col>바보</Col>
-    </Row>
+    <Wrapper>
+      <Row style={{ maxWidth: '1024px', width: '100%' }}>
+        <Col span={24}>
+          <Row justify="space-between" style={{ margin: '10px' }}>
+            <Col>
+              <img
+                src={'/assets/vietconnect-logo.png'}
+                style={{ width: '120px' }}
+              />
+            </Col>
+            <Col>
+              <Button>사장님 로그인</Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Wrapper>
   );
 }
