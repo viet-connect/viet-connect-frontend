3f59f6ee537222370923aef4f753559b192f849e
서현욱 <hyunwookkotra@gmail.com>
2023-04-27 19:53:17 +0900

    env setting

--- src/models/posting.ts
+++ src/models/posting.ts
@@ -32,13 +32,9 @@ export interface IPosting {
 
 export class Posting {
 	static async getPostingList(): Promise<any> {
-		console.log(process.env);
-
 		try {
 			const result = await fetch(
-				process.env.NODE_ENV === 'development'
-					? `${process.env.LOCAL_BASE_URL}/api/postings`
-					: `https://${process.env.VERCEL_URL}/api/postings`,
+				`${process.env.HOST}${process.env.VERCEL_URL}/api/postings`,
 			);
 
 			console.log('result', result);
