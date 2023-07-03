37e92145ae35e01f7cfeb64eb497b51f1e4f7fc0
hatae94 <gkxo5959@naver.com>
2023-07-03 23:21:24 +0900

    test deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient | undefined;
-};
+// eslint-disable-next-line import/no-mutable-exports
+let prisma;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient();
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
+	prisma = global.prisma;
+}
 
 export default prisma;
