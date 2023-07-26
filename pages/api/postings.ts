5ebaf8ab76dd59892e4f8d99e67cf5a60d4c08c6
hatae94 <gkxo5959@naver.com>
2023-07-26 22:10:09 +0900

    refactor: 포스팅 createdAt 내림차순 정렬

--- pages/api/postings.ts
+++ pages/api/postings.ts
@@ -13,7 +13,7 @@ export default async function posting_list(
 				const postingList = await prisma.posting.findMany({
 					orderBy: [
 						{
-							updatedAt: 'desc',
+							createdAt: 'desc',
 						},
 					],
 				});
