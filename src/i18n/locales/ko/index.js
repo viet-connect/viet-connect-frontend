377bd3f046115a646f6a589c5e59ed61551ff75f
hatae94 <gkxo5959@naver.com>
2023-08-22 00:40:53 +0900

    feat: 번역 기본 폴더구조 추가

---
+++ src/i18n/locales/ko/index.js
@@ -0,0 +1,11 @@
+import jobTable from './job_table.json';
+import opening from './opening.json';
+import posting from './posting.json';
+import navigation from './navigation.json';
+
+export default {
+	jobTable,
+	opening,
+	posting,
+	navigation,
+};
