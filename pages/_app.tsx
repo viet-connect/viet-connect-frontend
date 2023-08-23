e49d3db8705ca6f3d5a198b79f5dc097378922eb
서현욱 <hyunwookkotra@gmail.com>
2023-08-23 22:33:42 +0900

    feat: analytics package 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+// eslint-disable-next-line import/no-unresolved
+import { Analytics } from '@vercel/analytics/react';
 import '../src/config/recoil';
 
 import '../src/styles/globals.css';
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
 		<RecoilRoot>
 			<Component {...pageProps} />
+			<Analytics />
 		</RecoilRoot>
 	);
 }
