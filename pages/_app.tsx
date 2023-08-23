415a76416529fe36188f88ca6ca5727d4dd3f232
서현욱 <hyunwookkotra@gmail.com>
2023-08-23 22:44:00 +0900

    fix: analytics 주입

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import type { AppProps } from 'next/app';
-// eslint-disable-next-line import/no-unresolved
-import { Analytics } from '@vercel/analytics/react';
 import '../src/config/recoil';
 
 import '../src/styles/globals.css';
@@ -11,7 +9,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
 		<RecoilRoot>
 			<Component {...pageProps} />
-			<Analytics />
 		</RecoilRoot>
 	);
 }
