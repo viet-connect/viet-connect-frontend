b39ee554f137aad07ca7798260561bc0af7be2bc
hatae94 <gkxo5959@naver.com>
2023-10-05 02:22:24 +0900

    chore: 네이버 지도 url 변경

--- src/components/common/Map/index.tsx
+++ src/components/common/Map/index.tsx
@@ -11,7 +11,7 @@ export default function Map({ contactName, location }) {
         const options = { center: position, zoom: 19 };
         const map = new MapMaker('map', options);
         Event.once(map, 'init', () => {
-            const mapLink = `https://map.naver.com/p?title=${contactName}&lng=${lng}&lat=${lat}&zoom=19`;
+            const mapLink = `https://map.naver.com?title=${contactName}&lng=${lng}&lat=${lat}&zoom=19`;
             const svgTag = '<svg fill="#000000" width="36px" height="36px" viewBox="0 0 24 24" id="turn-right-direction-circle" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#808080" stroke-width="0.336"><circle id="primary" cx="12" cy="12" r="10" style="fill: #ffffff;"></circle><path id="secondary" d="M16.71,9.29l-2-2a1,1,0,1,0-1.42,1.42l.3.29H11a2,2,0,0,0-2,2v5a1,1,0,0,0,2,0V11h2.59l-.3.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l2-2A1,1,0,0,0,16.71,9.29Z" style="fill: #2c81ff;"></path></g><g id="SVGRepo_iconCarrier"><circle class="circle" id="primary" cx="12" cy="12" r="10" style="fill: #ffffff;"></circle><path id="secondary" d="M16.71,9.29l-2-2a1,1,0,1,0-1.42,1.42l.3.29H11a2,2,0,0,0-2,2v5a1,1,0,0,0,2,0V11h2.59l-.3.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l2-2A1,1,0,0,0,16.71,9.29Z" style="fill: #2c81ff;"></path></g></svg>';
             const shortCutEl = `<a target="_blank" href="${mapLink}">${svgTag}</a>`;
             const shortCutControl = new CustomControl(shortCutEl, { position: Position.TOP_RIGHT });
