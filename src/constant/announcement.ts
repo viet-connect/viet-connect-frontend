d951470c6d18356e4565282eefda07da0b7d8412
hatae94 <gkxo5959@naver.com>
2023-10-29 19:39:04 +0900

    feat: 공지사항 상수 임시 추가

---
+++ src/constant/announcement.ts
@@ -0,0 +1,53 @@
+export const posts = {
+    announcement1: {
+        id: 'announcement1',
+        category: '📢 공지사항',
+        author: '비엣커넥트',
+        title: 'Học tiếng Hàn Quốc qua các câu nói nổi tiếng trong phim 1 - The Glory (드라마 명대사로 한국어 공부하기 1 - 더글로리)',
+        descs: [
+            '• “오늘부터 모든 날이 흉흉할 거야. 자극적이고 끔찍할 거야. 막을 수도, 없앨 수도 없을 거야.”\n“Từ hôm nay, mỗi ngày đều đen tối và đáng sợ.\nNó có thể bị ngăn cản hoặc không thể bị xóa bỏ.”\n•“너 이거 우연 아니구나? 여기까지 오는데 우연은 단 한줄도 없었어.”\n“Cậu không phải tình cờ đến đây chứ? Không có chút tình cờ nào cả.”\n•“나는 바둑을 빨리 배웠어, 연진아. 목적이 분명했고, 상대가 정성껏 지은\n집을 빼앗으면 이기는 게임이라니. 아름답더라.”\n“Tôi đã học cờ vua rất nhanh, Yeon-jin ơi. Nó có mục đích rõ ràng và là trò\nchơi mà khi chiếm được ngôi nhà mà đối thủ đã xây dựng với tâm huyết\nthì sẽ thắng. Nó rất đẹp.”\n',
+            '• “사과하지마. 사과받자고 10대도, 20대도, 30대도 다 걸었을까. 넌 벌 받아야지. 신이 널 도우면 형벌, 신이 날 도우면 천벌.”\n“Đừng xin lỗi. Liệu có ai trong số những người từ 10 đến 30 tuổi đã từng\nxin lỗi và nhận lỗi chưa? Cậu phải trả giá cho việc mình đã làm sai. Nếu\nThượng Đế giúp cậu, thì đó là án phạt; nếu Ngài giúp tôi, thì đó là thiên hạ.”\n•“난 네가 시들어가는 이 순간이 아주 길었으면 좋겠거든. 우리 같이 천천히 말라 죽어 보자 연진아.”\n“Tôi muốn khoảnh khắc mà cậu héo úa kéo dài mãi mãi. Hãy cùng nhau khô\nhéo chậm rãi Yeon-jin ơi.”',
+            '• “근데 선배, 난 왕자님은 필요 없어요. 난 왕자가 아니라 나랑 같이 칼춤 춰줄 망나니가 필요하거든요.”\n“Nhưng Seon-bae, tôi không cần hoàng tử cả. Tôi chỉ cần một kẻ hát mãi với tôi.”\n•“네 딸은 거꾸로 보는 세상을 좋아하는 거. 세상이 거꾸로인 순간엔 모든\n색이 헷갈려도 이해 받기 때문일까. 단 하루도 잊어본 적이 없어. 어떤\n증오는 그리움을 닮아서 멈출 수가 없거든.”\n"Con gái của cậu thích nhìn thế giới ngược lại vì khi thế giới ngược”',
+        ],
+        images: [
+            '/image1-1.png',
+            '/image1-2.png',
+            '/image1-3.png',
+        ],
+        createdAt: '2023-10-10T16:00:13.383Z',
+    },
+    announcement2: {
+        id: 'announcement2',
+        category: '📢 공지사항',
+        author: '비엣커넥트',
+        title: 'Học tiếng Hàn Quốc qua các câu nói nổi tiếng trong phim 1 - The Glory (드라마 명대사로 한국어 공부하기 1 - 더글로리)',
+        descs: [
+            '• “오늘부터 모든 날이 흉흉할 거야. 자극적이고 끔찍할 거야. 막을 수도, 없앨 수도 없을 거야.”\n“Từ hôm nay, mỗi ngày đều đen tối và đáng sợ.\nNó có thể bị ngăn cản hoặc không thể bị xóa bỏ.”\n•“너 이거 우연 아니구나? 여기까지 오는데 우연은 단 한줄도 없었어.”\n“Cậu không phải tình cờ đến đây chứ? Không có chút tình cờ nào cả.”\n•“나는 바둑을 빨리 배웠어, 연진아. 목적이 분명했고, 상대가 정성껏 지은\n집을 빼앗으면 이기는 게임이라니. 아름답더라.”\n“Tôi đã học cờ vua rất nhanh, Yeon-jin ơi. Nó có mục đích rõ ràng và là trò\nchơi mà khi chiếm được ngôi nhà mà đối thủ đã xây dựng với tâm huyết\nthì sẽ thắng. Nó rất đẹp.”\n',
+            '• “사과하지마. 사과받자고 10대도, 20대도, 30대도 다 걸었을까. 넌 벌 받아야지. 신이 널 도우면 형벌, 신이 날 도우면 천벌.”\n“Đừng xin lỗi. Liệu có ai trong số những người từ 10 đến 30 tuổi đã từng\nxin lỗi và nhận lỗi chưa? Cậu phải trả giá cho việc mình đã làm sai. Nếu\nThượng Đế giúp cậu, thì đó là án phạt; nếu Ngài giúp tôi, thì đó là thiên hạ.”\n•“난 네가 시들어가는 이 순간이 아주 길었으면 좋겠거든. 우리 같이 천천히 말라 죽어 보자 연진아.”\n“Tôi muốn khoảnh khắc mà cậu héo úa kéo dài mãi mãi. Hãy cùng nhau khô\nhéo chậm rãi Yeon-jin ơi.”',
+            '• “근데 선배, 난 왕자님은 필요 없어요. 난 왕자가 아니라 나랑 같이 칼춤 춰줄 망나니가 필요하거든요.”\n“Nhưng Seon-bae, tôi không cần hoàng tử cả. Tôi chỉ cần một kẻ hát mãi với tôi.”\n•“네 딸은 거꾸로 보는 세상을 좋아하는 거. 세상이 거꾸로인 순간엔 모든\n색이 헷갈려도 이해 받기 때문일까. 단 하루도 잊어본 적이 없어. 어떤\n증오는 그리움을 닮아서 멈출 수가 없거든.”\n"Con gái của cậu thích nhìn thế giới ngược lại vì khi thế giới ngược”',
+        ],
+        images: [
+            '/image1-1.png',
+            '/image1-2.png',
+            '/image1-3.png',
+        ],
+        createdAt: '2023-10-12T16:00:13.383Z',
+    },
+    announcement3: {
+        id: 'announcement3',
+        category: '📢 공지사항',
+        author: '비엣커넥트',
+        title: 'Học tiếng Hàn Quốc qua các câu nói nổi tiếng trong phim 1 - The Glory (드라마 명대사로 한국어 공부하기 1 - 더글로리)',
+        descs: [
+            '• “오늘부터 모든 날이 흉흉할 거야. 자극적이고 끔찍할 거야. 막을 수도, 없앨 수도 없을 거야.”\n“Từ hôm nay, mỗi ngày đều đen tối và đáng sợ.\nNó có thể bị ngăn cản hoặc không thể bị xóa bỏ.”\n•“너 이거 우연 아니구나? 여기까지 오는데 우연은 단 한줄도 없었어.”\n“Cậu không phải tình cờ đến đây chứ? Không có chút tình cờ nào cả.”\n•“나는 바둑을 빨리 배웠어, 연진아. 목적이 분명했고, 상대가 정성껏 지은\n집을 빼앗으면 이기는 게임이라니. 아름답더라.”\n“Tôi đã học cờ vua rất nhanh, Yeon-jin ơi. Nó có mục đích rõ ràng và là trò\nchơi mà khi chiếm được ngôi nhà mà đối thủ đã xây dựng với tâm huyết\nthì sẽ thắng. Nó rất đẹp.”\n',
+            '• “사과하지마. 사과받자고 10대도, 20대도, 30대도 다 걸었을까. 넌 벌 받아야지. 신이 널 도우면 형벌, 신이 날 도우면 천벌.”\n“Đừng xin lỗi. Liệu có ai trong số những người từ 10 đến 30 tuổi đã từng\nxin lỗi và nhận lỗi chưa? Cậu phải trả giá cho việc mình đã làm sai. Nếu\nThượng Đế giúp cậu, thì đó là án phạt; nếu Ngài giúp tôi, thì đó là thiên hạ.”\n•“난 네가 시들어가는 이 순간이 아주 길었으면 좋겠거든. 우리 같이 천천히 말라 죽어 보자 연진아.”\n“Tôi muốn khoảnh khắc mà cậu héo úa kéo dài mãi mãi. Hãy cùng nhau khô\nhéo chậm rãi Yeon-jin ơi.”',
+            '• “근데 선배, 난 왕자님은 필요 없어요. 난 왕자가 아니라 나랑 같이 칼춤 춰줄 망나니가 필요하거든요.”\n“Nhưng Seon-bae, tôi không cần hoàng tử cả. Tôi chỉ cần một kẻ hát mãi với tôi.”\n•“네 딸은 거꾸로 보는 세상을 좋아하는 거. 세상이 거꾸로인 순간엔 모든\n색이 헷갈려도 이해 받기 때문일까. 단 하루도 잊어본 적이 없어. 어떤\n증오는 그리움을 닮아서 멈출 수가 없거든.”\n"Con gái của cậu thích nhìn thế giới ngược lại vì khi thế giới ngược”',
+        ],
+        images: [
+            '/image1-1.png',
+            '/image1-2.png',
+            '/image1-3.png',
+        ],
+        createdAt: '2023-10-13T16:00:13.383Z',
+    },
+};
