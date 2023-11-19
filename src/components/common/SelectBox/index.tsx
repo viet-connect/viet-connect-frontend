76a8570f760775d8d1f4200a1520aee45b62bacb
hatae94 <gkxo5959@naver.com>
2023-11-20 01:26:29 +0900

    feat: select-box onChange 적용

--- src/components/common/SelectBox/index.tsx
+++ src/components/common/SelectBox/index.tsx
@@ -9,11 +9,12 @@ interface Option {
 interface ISelectBoxProps {
 	options: Option[];
 	initialValue: string;
+	onChange: (value: string) => void;
 }
 
-export default function SelectBox({ options, initialValue }: ISelectBoxProps) {
+export default function SelectBox({ options, initialValue, onChange }: ISelectBoxProps) {
 	return (
-		<Select>
+		<Select onChange={(e) => onChange(e.target.value)}>
 			{options.map((option: Option) => (
 				<option
 					key={option.value}
@@ -30,11 +31,12 @@ export default function SelectBox({ options, initialValue }: ISelectBoxProps) {
 const Select = styled.select`
 	max-width: 100%;
 	min-height: 1px;
-	width: 100%;
-	padding: 3px 11px 3px 11px;
+	width: auto;
+	padding: 3px 14px 3px 14px;
 	background-color: transparent;
 	:hover {
-		border-color: lightblue;
+		border-color: #448ef7;
 	}
-	border: 1px solid black;
+	border: 1px solid rgba(128,128,128,0.5);
+	border-radius: 6px;
 `;
