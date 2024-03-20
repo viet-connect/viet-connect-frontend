98898ef6c0d50fc545c460455cdb4fcb2ab5a148
hatae94 <gkxo5959@naver.com>
2024-03-20 20:41:50 +0900

    feat: 지역 선택시 세부지역 리스트 추가

--- src/components/home/filter/index.tsx
+++ src/components/home/filter/index.tsx
@@ -93,6 +93,7 @@ export default function HomeFilter() {
 
 				if (isQuerySetting) {
 					setIsQuerySetting(true);
+					setSelectedDistrictArray(['defaultProvince', ...target]);
 					return;
 				}
 
