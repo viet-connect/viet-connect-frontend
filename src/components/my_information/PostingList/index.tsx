e2fc6efb10869da1f05e819efe544d2cfb89db66
hatae94 <gkxo5959@naver.com>
2024-05-08 23:38:28 +0900

    feat: 지원한/받은 공고 페이지 추가

---
+++ src/components/my_information/PostingList/index.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export default function PostingList(props) {
+  const { list = [] } = props;
+  return (
+    <Container>
+      {list.map(({ contactName, name, phone }, i) => (
+        <ListItem key={i} $last={i === list.length - 1}>
+          <div className="list__contact-name">{contactName}</div>
+          {/* TODO: 조건에 따른 블러 on/off 적용 */}
+          {name && phone && <div>{`${name}: ${phone}`}</div>}
+        </ListItem>
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+interface ListItemProps {
+  $last: boolean;
+}
+
+const ListItem = styled.div<ListItemProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 8px;
+  padding: 12px 4px;
+  min-height: 40px;
+  border-bottom: ${({ $last }) => ($last ? 'none' : '1px solid rgba(128, 128, 128, 0.5)')};
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
+
+  .list {
+    &__contact-name {
+      font-weight: bold;
+    }
+  }
+`;
--- src/components/my_information/index.tsx
+++ src/components/my_information/index.tsx
@@ -1,16 +1,19 @@
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 import BasicInfo from './BasicInfo';
 import ConditionalInfo from './ConditionalInfo';
 import CommonButton from '../common/Button';
 import { IUser, User } from '../../models/user';
+import Toggle from '../common/Toggle';
+import RadioBox from '../common/RadioBox';
+import PostingList from './PostingList';
 
 export default function MyInformation({ data }) {
   const { data: sesstionData } = useSession();
   const [info, setInfo] = useState({
-    image: '',
     name: '',
     nation: null,
     gender: null,
@@ -23,6 +26,10 @@ export default function MyInformation({ data }) {
     selfIntroduction: '',
     ...data,
   });
+  const [active, setActive] = useState(false);
+  const [subPage, setSubPage] = useState('privacy');
+
+  const { t } = useTranslation();
 
   const onChange = (value) => {
     setInfo({ ...info, ...value });
@@ -36,6 +43,9 @@ export default function MyInformation({ data }) {
     }, {});
     User.handleUpdateUser(sesstionData?.user?.id, validInfo as IUser);
   };
+
+  const onToggle = (checked) => setActive(checked);
+
   return (
     <Container>
       <ImageWrapper>
@@ -45,24 +55,112 @@ export default function MyInformation({ data }) {
           <div className="empty-image" />
         )}
       </ImageWrapper>
-      <BasicInfo {...info} onChange={onChange} />
-      <ConditionalInfo {...info} onChange={onChange} />
-      <CommonButton
-        className="save-button"
-        label="저장하기"
-        extraWrapperStyle={{ height: 43, color: 'white', backgroundColor: '#1890ff' }}
-        onClick={saveInfo}
-      />
+      <PageControlWrapper>
+        <RadioBox
+          color="#F0F0F0"
+          value={subPage}
+          options={[
+            { label: t('myPage:privacy'), value: 'privacy' },
+            { label: t('myPage:postingInformation'), value: 'postingInformation' },
+          ]}
+          onChange={(v) => setSubPage(v)}
+        />
+      </PageControlWrapper>
+      {subPage === 'privacy' ? (
+        <PrivacyConatiner>
+          <BasicInfo {...info} onChange={onChange} />
+          <ConditionalInfo {...info} onChange={onChange} />
+          <CommonButton
+            className="save-button"
+            label="저장하기"
+            extraWrapperStyle={{ height: 43, color: 'white', backgroundColor: '#1890ff' }}
+            onClick={saveInfo}
+          />
+        </PrivacyConatiner>
+      ) : (
+        <>
+          <Toggle
+            label={active ? t('myPage:supportedPostings') : t('myPage:appliedPostings')}
+            value={active}
+            onChange={onToggle}
+          />
+          {/* TODO: DB 데이터 적용 */}
+          <PostingList
+            list={
+              active
+                ? [
+                    {
+                      id: '지원받은 공고 id1',
+                      contactName: '지원받은 별미곱창',
+                      name: '태태태',
+                      phone: '010-1234-1234',
+                    },
+                    {
+                      id: '지원받은 공고 id2',
+                      contactName: '지원받은 별미곱창2',
+                      name: '태태태',
+                      phone: '010-1234-1234',
+                    },
+                    {
+                      id: '지원받은 공고 id3',
+                      contactName: '지원받은 별미곱창3',
+                      name: '태태태',
+                      phone: '010-1234-1234',
+                    },
+                    {
+                      id: '지원받은 공고 id4',
+                      contactName: '지원받은 별미곱창4',
+                      name: '태태태',
+                      phone: '010-1234-1234',
+                    },
+                  ]
+                : [
+                    {
+                      id: '지원한 공고 id1',
+                      contactName: '지원한 별미곱창',
+                    },
+                    {
+                      id: '지원한 공고 id2',
+                      contactName: '지원한 별미곱창2',
+                    },
+                    {
+                      id: '지원한 공고 id3',
+                      contactName: '지원한 별미곱창3',
+                    },
+                    {
+                      id: '지원한 공고 id4',
+                      contactName: '지원한 별미곱창4',
+                    },
+                  ]
+            }
+          />
+        </>
+      )}
     </Container>
   );
 }
 
-const Container = styled.div`
+const PageControlWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .radio-box {
+    width: 300px;
+    height: 40px;
+  }
+`;
+
+const PrivacyConatiner = styled.div`
   display: flex;
   flex-direction: column;
   gap: 48px;
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+`;
 const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
