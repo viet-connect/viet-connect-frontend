4bbf3a8d068c794ea8d0efd85923d1d714cf95aa
hatae94 <gkxo5959@naver.com>
2024-06-03 00:20:09 +0900

    fix: getSession -> getServerSession

--- pages/my_information/index.tsx
+++ pages/my_information/index.tsx
@@ -1,8 +1,9 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { getSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
 import Layout from '../../src/components/common/Layout';
 import MyInformationPage from '../../src/components/my_information';
 import { User } from '../../src/models/user';
+import { authOptions } from '../api/auth/[...nextauth]';
 
 export default function MyInformation({ data }) {
   return <Layout pageIndex={4}>{data ? <MyInformationPage data={data} /> : <div></div>}</Layout>;
@@ -10,9 +11,9 @@ export default function MyInformation({ data }) {
 
 export async function getServerSideProps(context) {
   const { locale } = context;
-  const session = (await getSession(context)) ?? { user: null };
   const i18n = ['common', 'detail', 'jobTable', 'navigation', 'opening', 'posting', 'login', 'myPage'];
   const translation = await serverSideTranslations(locale, i18n);
+  const session = await getServerSession(context.req, context.res, authOptions);
   let data = null;
   if (session.user) {
     data = await User.getUserInfo(session.user.id);
