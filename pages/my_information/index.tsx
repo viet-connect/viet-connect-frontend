34e6b76e73420d542186cc56aff2d95a0d715ce4
서현욱 <hyunwookkotra@gmail.com>
2024-06-02 21:56:32 +0700

    mypage 안나오는에러 임시조치

--- pages/my_information/index.tsx
+++ pages/my_information/index.tsx
@@ -5,18 +5,17 @@ import MyInformationPage from '../../src/components/my_information';
 import { User } from '../../src/models/user';
 
 export default function MyInformation({ data }) {
-  return (
-    <Layout pageIndex={4}>
-      <MyInformationPage data={data} />
-    </Layout>
-  );
+  return <Layout pageIndex={4}>{data ? <MyInformationPage data={data} /> : <div></div>}</Layout>;
 }
 
 export async function getServerSideProps(context) {
   const { locale } = context;
-  const { user } = await getSession(context);
+  const session = await getSession(context);
   const i18n = ['common', 'detail', 'jobTable', 'navigation', 'opening', 'posting', 'login', 'myPage'];
   const translation = await serverSideTranslations(locale, i18n);
-  const data = await User.getUserInfo(user.id);
+  let data = null;
+  if (session.user.id) {
+    data = await User.getUserInfo(session.user.id);
+  }
   return { props: { ...translation, data } };
 }
