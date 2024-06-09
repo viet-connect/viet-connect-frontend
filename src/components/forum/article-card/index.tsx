d10ebd348a994eea8d6e63e822e65857b30a75a1
서현욱 <hyunwookkotra@gmail.com>
2024-06-09 08:40:45 +0700

    서비스 소개글 고정

--- src/components/forum/article-card/index.tsx
+++ src/components/forum/article-card/index.tsx
@@ -6,74 +6,79 @@ import DateUtils from '../../../utils/DateUtils';
 import Badge from '../../common/Badge';
 
 export default function ArticleCard({ article, announcement }) {
-    const router = useRouter();
-    const { t } = useTranslation();
+  const router = useRouter();
+  const { t } = useTranslation();
 
-    const { id, author, category: _category, title, viewCount, createdAt } = article;
-    const date = createdAt ? DateUtils.getFullDateString(createdAt) : '';
-    const category = t(`article:${_category}`);
+  const { id, author, category: _category, title, viewCount, createdAt } = article;
+  const date = createdAt ? DateUtils.getFullDateString(createdAt) : '';
+  const category = t(`article:${_category}`);
 
-    const onClick = () => {
-        router.push(`forum/detail/${id}`);
-    };
-    return (
-        <Wrapper className="article" announcement onClick={onClick}>
-            <div className="article__category">{category}</div>
-            {date && (<div className="article__date">{date}</div>)}
-            <div className="article__title">{title}</div>
-            <div className="article__author">{`작성자: ${author}`}</div>
-            {viewCount && (<div className="article__view-count">{`조회 ${viewCount}`}</div>)}
-        </Wrapper>
-    );
+  const onClick = () => {
+    router.push(`forum/detail/${id}`);
+  };
+
+  /* 임시방편 ID가 anchor일 때는 버튼생성 */
+  return (
+    <Wrapper className="article" announcement onClick={onClick}>
+      <div className="article__category">{category}</div>
+      {id !== 'anchor' && date && <div className="article__date">{date}</div>}
+      <div className="article__title">{title}</div>
+      <div className="article__author">{`작성자: ${author}`}</div>
+      {viewCount && <div className="article__view-count">{`조회 ${viewCount}`}</div>}
+    </Wrapper>
+  );
 }
 
 interface hoverProps {
-    announcement: Boolean;
+  announcement: Boolean;
 }
 
 const Wrapper = styled.div<hoverProps>`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 8px;
-    padding: 8px;
-    border-bottom: 1px solid rgba(5, 5, 5, 0.06);
-    cursor: pointer;
-    &:hover {
-        ${(props) =>
-            props.announcement ?
-            css`
-                background-color: rgb(234,123,20, 0.1);
-            ` : css`
-                background-color: #f1f1f1;
-            `
-        }
-		
-	}
-    
-    .article {
-        &__category, &__title {
-            font-size: 13px;
-        }
-        &__title {
-            grid-column: span 4;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-        }
-        &__title {
-            font-weight: bold;
-        }
-        &__category {
-            grid-column: 1 / 4;
-        }
-        &__author, &__date, &__view-count {
-            font-size: 12px;
-        }
-        &__author {
-            grid-column: 1 / 4;
-        }
-        &__date, &__view-count {
-            text-align: right;
-        }
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 8px;
+  padding: 8px;
+  border-bottom: 1px solid rgba(5, 5, 5, 0.06);
+  cursor: pointer;
+  &:hover {
+    ${(props) =>
+      props.announcement
+        ? css`
+            background-color: rgb(234, 123, 20, 0.1);
+          `
+        : css`
+            background-color: #f1f1f1;
+          `}
+  }
+
+  .article {
+    &__category,
+    &__title {
+      font-size: 13px;
     }
+    &__title {
+      grid-column: span 4;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    &__title {
+      font-weight: bold;
+    }
+    &__category {
+      grid-column: 1 / 4;
+    }
+    &__author,
+    &__date,
+    &__view-count {
+      font-size: 12px;
+    }
+    &__author {
+      grid-column: 1 / 4;
+    }
+    &__date,
+    &__view-count {
+      text-align: right;
+    }
+  }
 `;
--- src/components/forum/article-post/index.tsx
+++ src/components/forum/article-post/index.tsx
@@ -2,80 +2,82 @@ import styled from 'styled-components';
 import { useTranslation } from 'next-i18next';
 import CommonInput from '../../common/Input';
 import Editor from '../../common/Editor';
-import CommonButton from '../../common/Button';
 
 export default function ArticlePost(props) {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const { article, readOnly, onChange, children } = props;
-    const { title, contents, category } = article;
+  const { article, readOnly, onChange, children } = props;
+  const { title, contents } = article;
 
-    const titleAttrs = {
-        className: `article-post__title${readOnly ? '--readonly' : ''}`,
-        type: 'textarea',
-        value: title,
-        placeholder: t('article:title'),
-        wrapperStyle: { fontSize: '24px' },
-        maxLength: 100,
-        rows: 1,
-        wrap: 'on',
-        readOnly,
-    };
-    const editorAttrs = {
-        value: contents,
-        placeholder: t('article:editor'),
-        readOnly,
-    };
+  const titleAttrs = {
+    className: `article-post__title${readOnly ? '--readonly' : ''}`,
+    type: 'textarea',
+    value: title,
+    placeholder: t('article:title'),
+    wrapperStyle: { fontSize: '24px' },
+    maxLength: 100,
+    rows: 1,
+    wrap: 'on',
+    readOnly,
+  };
+  const editorAttrs = {
+    value: contents,
+    placeholder: t('article:editor'),
+    readOnly,
+  };
 
-    return (
-        <Wrapper className='article-post'>
-            <HeadWrapper>
-                <CommonInput { ...titleAttrs } onChange={(v) => onChange({ title: v })}/>
-                {children}
-            </HeadWrapper>
-            <Editor { ...editorAttrs } onChange={(v) => onChange({ contents: v })}/>
-        </Wrapper>
-    );
+  /* 임시방편 ID가 anchor일 때는 버튼생성 */
+  return (
+    <Wrapper className="article-post">
+      <HeadWrapper>
+        <CommonInput {...titleAttrs} onChange={(v) => onChange({ title: v })} />
+        {children}
+      </HeadWrapper>
+      <Editor {...editorAttrs} onChange={(v) => onChange({ contents: v })} />
+      {editorAttrs.readOnly && props.article.id === 'anchor' && <button>wsss</button>}
+    </Wrapper>
+  );
 }
 
 const HeadWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 4px;
-    position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  position: relative;
 
-    .article-post__title {
-        margin-top: 70px;
-        textarea {
-            padding: 8px 0px 14px 0px !important;
-        }
-        &--readonly {
-            padding: 8px 0px;
-
-            input, textarea {
-                border: none !important;
-            }
-        }
-
-        @media(min-width: 410px) {
-            margin-top: 48px;
-        }
+  .article-post__title {
+    margin-top: 70px;
+    textarea {
+      padding: 8px 0px 14px 0px !important;
     }
+    &--readonly {
+      padding: 8px 0px;
 
-    .common-editor {
+      input,
+      textarea {
         border: none !important;
-        &__toolbar {
-            position: absolute;
-            top: 0px;
-            width: 100%;
-            border: 1px solid #d9d9d9;
-            border-radius: 6px;
-            box-sizing: border-box;
-        }
+      }
     }
+
+    @media (min-width: 410px) {
+      margin-top: 48px;
+    }
+  }
+
+  .common-editor {
+    border: none !important;
+    &__toolbar {
+      position: absolute;
+      top: 0px;
+      width: 100%;
+      border: 1px solid #d9d9d9;
+      border-radius: 6px;
+      box-sizing: border-box;
+    }
+  }
 `;
