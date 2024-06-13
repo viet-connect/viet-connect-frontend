94bec88d023812526933110f2fd38ade736c2adb
서현욱 <hyunwookkotra@gmail.com>
2024-06-13 21:06:17 +0700

    채널연결

---
+++ src/components/common/KakaoButton/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import CommonButton from '../Button';
+
+const KakaoChatButton = () => {
+  useEffect(() => {
+    if (!window.Kakao) {
+      const script = document.createElement('script');
+      script.src = 'https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js';
+      script.integrity = 'sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4';
+      script.crossOrigin = 'anonymous';
+      script.onload = () => {
+        const key = process.env.NEXT_PUBLIC_KAKAO_KEY;
+        window.Kakao.init(key);
+      };
+
+      document.head.appendChild(script);
+    }
+  }, []);
+
+  const kakaoChat = () => {
+    if (window.Kakao) {
+      console.log('Kakao SDK is loaded.');
+      try {
+        window.Kakao.Channel.chat({
+          channelPublicId: '_XtVUG',
+        });
+      } catch (error) {
+        console.error('Error in Kakao chat:', error);
+      }
+    } else {
+      console.error('Kakao SDK is not loaded.');
+    }
+  };
+
+  return (
+    <CommonButton
+      className="redirect-channel"
+      label="카카오톡 1:1 문의"
+      extraWrapperStyle={{ height: 43, color: 'black', backgroundColor: '#F7E600' }}
+      onClick={kakaoChat}
+    />
+  );
+};
+
+export default KakaoChatButton;
