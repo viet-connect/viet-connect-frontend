9efffa118fd6f80c0dc8c2550cdab646a7424430
hatae94 <gkxo5959@naver.com>
2024-06-26 23:16:08 +0900

    chore: 애드센스 기존 컴포넌트 대체

--- src/components/home/job_list/index.tsx
+++ src/components/home/job_list/index.tsx
@@ -1,40 +1,40 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 import JobCard from './job_card';
-import DisplayAds from '../../common/DisplayAdd';
+import GoogleAd from '../../common/GoogleAd';
 
 export default function JobList({ tableContent }) {
-	const onClick = () => {
-		window.open('https://vt.beemall.shop', '_blank');
-	};
-	return (
-		<JobListWrapper>
-			<DisplayAds />
-			{/* <Banner onClick={onClick}>
+  const onClick = () => {
+    window.open('https://vt.beemall.shop', '_blank');
+  };
+  return (
+    <JobListWrapper>
+      <GoogleAd />
+      {/* <Banner onClick={onClick}>
 				<Image alt="banner" src="/beemall_banner.png" fill />
 			</Banner> */}
-			{tableContent.map((content) => (
-				<JobCard key={content.id} content={content} />
-			))}
-		</JobListWrapper>
-	);
+      {tableContent.map((content) => (
+        <JobCard key={content.id} content={content} />
+      ))}
+    </JobListWrapper>
+  );
 }
 
 const Banner = styled.div`
-	position: relative;
-	width: 100%;
-	height: 104px;
-	gap: 5px;
-	cursor: pointer;
+  position: relative;
+  width: 100%;
+  height: 104px;
+  gap: 5px;
+  cursor: pointer;
 
-	img {
-		object-fit: fit;
-		border-radius: 6px;
-	}
+  img {
+    object-fit: fit;
+    border-radius: 6px;
+  }
 `;
 
 const JobListWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
