11fe371e98c4beedd3cd1a13ec43f0185423b28d
서현욱 <hyunwookkotra@gmail.com>
2024-07-14 18:33:35 +0700

    url 수정

--- pages/sitemap/job_opening-sitemap.xml.tsx
+++ pages/sitemap/job_opening-sitemap.xml.tsx
@@ -8,7 +8,7 @@ export const getServerSideProps = async (context) => {
       ? `${process.env.NEXT_PUBLIC_HOST}${process.env.NEXT_PUBLIC_VERCEL_URL}`
       : process.env.DEPLOY_URL;
   const sitemapFields: ISitemapField[] = list.map(({ id }) => ({
-    loc: `${url}/job_opening/detail/${id}`, // 페이지 경로
+    loc: `${url}job_opening/detail/${id}`, // 페이지 경로
     lastmod: new Date().toISOString(), // 최근변경일자
     changefreq: 'daily', // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
     priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
