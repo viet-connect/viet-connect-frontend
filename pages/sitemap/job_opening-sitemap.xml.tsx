18cdbdf15277dff1acd641939bdcf658b3ec8811
서현욱 <hyunwookkotra@gmail.com>
2024-08-04 09:10:10 +0700

    사이트맵 베트남 추가

--- pages/sitemap/job_opening-sitemap.xml.tsx
+++ pages/sitemap/job_opening-sitemap.xml.tsx
@@ -19,14 +19,21 @@ export const getServerSideProps = async (context) => {
     jobList.push(...res[i].list);
   }
 
-  const sitemapFields: ISitemapField[] = jobList.map(({ id }) => ({
+  const koreaSitemapFields: ISitemapField[] = jobList.map(({ id }) => ({
     loc: `${url}job_opening/detail/${id}`, // 페이지 경로
     lastmod: new Date().toISOString(), // 최근변경일자
     changefreq: 'daily', // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
     priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
   }));
-  // console.log(sitemapFields);
 
+  const vietSitemapFields: ISitemapField[] = jobList.map(({ id }) => ({
+    loc: `${url}vn/job_opening/detail/${id}`, // 페이지 경로
+    lastmod: new Date().toISOString(), // 최근변경일자
+    changefreq: 'daily', // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
+    priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
+  }));
+
+  const sitemapFields = vietSitemapFields.concat(koreaSitemapFields);
   const sitemap = getServerSideSitemapLegacy(context, sitemapFields);
   return sitemap;
 };
